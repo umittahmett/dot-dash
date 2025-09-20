af2d064ebfdfb1b232dffdaf0c86e3338c128f2b
Ümit Ahmet <135972028+umittahmett@users.noreply.github.com>
2025-09-20 20:43:19 +0300

    feat: add Navbar component with star and settings buttons

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import Button from './ui/Button.vue';
+  import StarIcon from '@/assets/icons/star.svg';
+  import CogIcon from '@/assets/icons/cog.svg';
+</script>
+
+<template>
+  <div class="flex items-center justify-between gap-4">
+    <Button variant="secondary">
+      <StarIcon />
+    </Button>
+
+    <h1 class="text-xl font-bold">Dot Dash</h1>
+
+    <Button variant="secondary">
+      <CogIcon />
+    </Button>
+  </div>
+</template>
