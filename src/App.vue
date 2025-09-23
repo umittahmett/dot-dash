740d3b9c2754c2d694d1e86e6e018c32e08b8fe3
Ümit Ahmet <135972028+umittahmett@users.noreply.github.com>
2025-09-24 00:10:55 +0300

    refactor: remove iPhone frame and update UI layout with responsive design

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,34 @@
 <script setup lang="ts">
 
-import IPhoneFrame from '@/components/IPhoneFrame.vue';
 import Navbar from '@/components/Navbar.vue';
 import Converter from '@/components/converter.vue';
 import { useFileOperations } from '@/composables/useFileOperations';
 import clsx from 'clsx';
-import { watch } from 'vue';
 
 const { loading } = useFileOperations()
-
-watch(loading, (newLoading) =>{
-  console.log(newLoading)
-})
 
 </script>
 
 <template>
-  <div :class="clsx('flex justify-center items-center min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 p-8', 
-  {
-    'opacity-50 select-none': loading
-  })">
-    <IPhoneFrame>
-      <div class="space-y-5 w-full max-w-3xl">
-        <Navbar />
-        <Converter/>
+  <div :class="clsx('flex justify-center items-center min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 p-4 md:p-6 lg:p-8', 
+    {
+      'opacity-50 select-none': loading
+    })"
+  >
+    <div class="lg:rounded-[36px] lg:bg-black lg:p-5 w-full lg:max-w-7xl">
+      <div class="bg-background rounded-2xl p-5">
+        <div class="space-y-6 w-full">
+          <Navbar />
+          <Converter/>
+        </div>
       </div>
-    </IPhoneFrame>
+    </div>
   </div>
 
-
+  <!-- Spinner -->
   <div v-if="loading" class="fixed inset-0 bg-black/60">
     <div class="flex items-center justify-center h-screen">
       <div class="animate-spin rounded-full h-20 w-20 border-b border-t border-white"></div>
     </div>
-
   </div>
 </template>
--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -3,12 +3,13 @@
     <textarea 
       style="word-wrap: break-word; word-spacing: 6px;"
       :class="[
-        'py-2 px-4 font-bold bg-white min-h-48 rounded-lg shadow-[0_4px_20px_0_rgba(154, 154, 154, 0.25)] text-black placeholder:text-black/50 text-lg outline-0 resize-none',
+        'py-2 px-4 font-bold bg-white rounded-lg shadow-[0_4px_20px_0_rgba(154, 154, 154, 0.25)] text-black placeholder:text-black/50 text-lg outline-0 resize-none',
         {
           'bg-gray-100 opacity-60': disabled
         },
         extraClass
       ]"
+      rows="8"
       :disabled="disabled"
       v-model="value"
       v-bind="$attrs"
