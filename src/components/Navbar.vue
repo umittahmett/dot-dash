3d0892b7acadf68df2d6f9f932b53b8c2e82f085
Ümit Ahmet <135972028+umittahmett@users.noreply.github.com>
2025-09-24 00:22:52 +0300

    feat: update navbar icons and styling with new button sizes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
   import Button from './ui/Button.vue';
-  import StarIcon from '@/assets/icons/star.svg';
 </script>
 
 <template>
   <div class="flex items-center justify-between gap-4">
-    <Button variant="secondary">
-      <StarIcon />
+    <Button size="icon" variant="secondary">
+      <div class="rounded-full size-2 bg-white">
+
+      </div>
     </Button>
 
-    <h1 class="text-xl font-bold">Dot Dash</h1>
+    <h1 class="text-2xl font-bold">Dot Dash</h1>
 
-    <Button variant="secondary">
-      <StarIcon />
+    <Button size="icon" variant="secondary">
+      <div class="w-4 h-1 bg-white"></div>
     </Button>
   </div>
 </template>
