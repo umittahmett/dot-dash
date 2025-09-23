d1da30bd92f147df6be72838c3012537716b1993
Ümit Ahmet <135972028+umittahmett@users.noreply.github.com>
2025-09-23 23:19:24 +0300

    feat: add icon button sizes and support custom class injection in Button component

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     v-bind="$attrs"
-    class="cursor-pointer relative active:scale-95 mb-1 mr-1 disabled:cursor-not-allowed disabled:select-none disabled:active:scale-100 disabled:opacity-50">
+    :class="clsx('cursor-pointer relative active:scale-95 mb-1 mr-1 disabled:cursor-not-allowed disabled:select-none disabled:active:scale-100 disabled:opacity-50', extraClass)">
     <div :class="[
       'relative z-2 text-white border-2 border-black rounded-lg [&>svg]:size-6 flex items-center justify-center',
       {
@@ -10,7 +10,9 @@
       },
       {
         'px-2.5 py-1.5 text-sm gap-2': size === 'sm',
-        'px-5 py-3 text-2xl gap-2.5': size === 'lg'
+        'px-5 py-3 text-2xl gap-2.5': size === 'lg',
+        'w-[68px] h-[52px] text-2xl': size === 'icon',
+        'w-[36px] h-[36px] text-2xl': size === 'sm-icon'
       }
     ]">
 
@@ -22,13 +24,17 @@
 </template>
 
 <script setup lang="ts">
+import clsx from 'clsx';
+
 interface Props {
   variant?: 'primary' | 'secondary'
-  size?: 'sm' | 'lg'
+  size?: 'sm' | 'lg' | 'icon' | 'sm-icon'
+  extraClass?: string
 }
 
 withDefaults(defineProps<Props>(), {
   variant: 'primary',
-  size: 'lg'
+  size: 'lg',
+  extraClass: ''
 })
 </script>
